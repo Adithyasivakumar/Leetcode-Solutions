24f4b8e2d362c34928772b9396ce65e8cad9e28d
Adithyasivakumar <adithyasivakumar07@gmail.com>
2026-01-07 23:40:59 +0530

    Updated a new file

---
+++ 169. Majority Elements/LC-283 Move Zeros.py
@@ -0,0 +1,38 @@
+#Approach 1
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+
+        majority = None
+        count = 0
+
+        for num in nums:
+            
+            if count == 0:
+                majority = num 
+
+            if num == majority:
+                count += 1              
+
+            elif num != majority:
+                count -= 1
+        
+        return majority
+
+#Approach 2
+
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        
+        frequency_count = Counter(nums)
+        
+        for num, count in frequency_count.items():
+            
+            if count > n / 2:
+                return num
+        
+        return False
